84b326cc2d5f4b46bbb8148feaeb30210cc955a7
Denis <dddenisss35@gmail.com>
2025-07-13 14:46:18 +0300

    GameFramework.Pro

---
+++ GameFramework.Pro/GameFramework.Pro/GameFramework.Pro.Extensions/00.UI/RouterBase2.cs
@@ -0,0 +1,54 @@
+﻿namespace GameFramework.Pro.Extensions {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class RouterBase2<TTheme, TScreen, TApplication> : ProgramBase where TTheme : ThemeBase where TScreen : ScreenBase where TApplication : ApplicationBase {
+
+        private Func<TTheme> theme = default!;
+        private Func<TScreen> screen = default!;
+        private TApplication application = default!;
+
+        protected TTheme Theme {
+            get {
+                Assert.Operation.Valid( $"Theme must be non-null", this.theme != null );
+                return this.theme() ?? throw new InvalidOperationException( $"Theme must be non-null" );
+            }
+        }
+        protected Func<TTheme> Theme_ {
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.theme = value;
+            }
+        }
+        protected TScreen Screen {
+            get {
+                Assert.Operation.Valid( $"Theme must be non-null", this.screen != null );
+                return this.screen() ?? throw new InvalidOperationException( $"Screen must be non-null" );
+            }
+        }
+        protected Func<TScreen> Screen_ {
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.screen = value;
+            }
+        }
+        protected TApplication Application {
+            get {
+                Assert.Operation.Valid( $"Application must be non-null", this.application != null );
+                return this.application;
+            }
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.application = value;
+            }
+        }
+
+        public RouterBase2() {
+        }
+        public override void Dispose() {
+            base.Dispose();
+        }
+
+    }
+}
---
+++ GameFramework.Pro/GameFramework.Pro/GameFramework.Pro.Extensions/ProgramBase2.cs
@@ -0,0 +1,61 @@
+﻿namespace GameFramework.Pro.Extensions {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class ProgramBase2<TTheme, TScreen, TRouter, TApplication> : ProgramBase where TTheme : ThemeBase where TScreen : ScreenBase where TRouter : RouterBase where TApplication : ApplicationBase {
+
+        private TTheme theme = default!;
+        private TScreen screen = default!;
+        private TRouter router = default!;
+        private TApplication application = default!;
+
+        protected TTheme Theme {
+            get {
+                Assert.Operation.Valid( $"Theme must be non-null", this.theme != null );
+                return this.theme;
+            }
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.theme = value;
+            }
+        }
+        protected TScreen Screen {
+            get {
+                Assert.Operation.Valid( $"Screen must be non-null", this.screen != null );
+                return this.screen;
+            }
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.screen = value;
+            }
+        }
+        protected TRouter Router {
+            get {
+                Assert.Operation.Valid( $"Router must be non-null", this.router != null );
+                return this.router;
+            }
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.router = value;
+            }
+        }
+        protected TApplication Application {
+            get {
+                Assert.Operation.Valid( $"Application must be non-null", this.application != null );
+                return this.application;
+            }
+            init {
+                Assert.Argument.NotNull( $"Argument 'value' must be non-null", value != null );
+                this.application = value;
+            }
+        }
+
+        public ProgramBase2() {
+        }
+        public override void Dispose() {
+            base.Dispose();
+        }
+
+    }
+}
