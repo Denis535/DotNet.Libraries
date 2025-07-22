c030c7b71b8ffd1b49b53f9e398e72ab8a5c47d8
Denis <dddenisss35@gmail.com>
2025-07-22 16:13:54 +0300

    GameFramework.Pro

--- GameFramework.Pro/GameFramework.Pro/GameFramework.Pro/00.UI/01.Screen/ViewBase.cs
+++ GameFramework.Pro/GameFramework.Pro/GameFramework.Pro/00.UI/01.Screen/ViewBase.cs
@@ -3,7 +3,6 @@ namespace GameFramework.Pro {
     using System;
     using System.Collections.Generic;
     using System.Text;
-    using System.Threading;
 
     public abstract class ViewBase : DisposableBase, IView {
 
